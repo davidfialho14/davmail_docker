55ec387387a993ca3a41a68ee265cfdf033aaa50
David Fialho <fialho.david@protonmail.com>
2017-05-26 19:34:24 +0100

    Added a runner script with the URL of the exchange server hardcoded

---
+++ runner.sh
@@ -0,0 +1,12 @@
+URL="https://owa.uac.pt/ews/exchange.asmx"
+
+# Ensure an old container is removed
+docker rm davmail &> /dev/null
+
+# Run the new container
+docker run -d \
+  -p 1025:1025 \
+  -p 1110:1110 \
+  --name davmail \
+  -e EXCHANGE_URL="$URL" \
+  davmail
